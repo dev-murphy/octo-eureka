ceb6c3f25f309896f7746d01d410d42d3e74e17f
dev-murphy <developmentmurphy@gmail.com>
2024-10-02 13:01:31 -0500

    chore: add auto imports for vue directives and components, add web font download util

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
+import AutoRouter from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 import VueRouter from "unplugin-vue-router/vite";
+import { VueRouterAutoImports } from "unplugin-vue-router";
 import Vue from "@vitejs/plugin-vue";
+import WebfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +13,17 @@ export default defineConfig({
       extensions: [".vue"],
       dts: "src/typed-router.d.ts",
     }),
+    AutoRouter({
+      imports: ["vue", VueRouterAutoImports],
+      dts: "src/auto-imports.d.ts",
+      dirs: ["src/stores"],
+    }),
+    Components({
+      extensions: ["vue"],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: "src/components.d.ts",
+    }),
+    WebfontDownload(),
     Vue(),
   ],
 });
