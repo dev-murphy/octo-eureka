709dcb4ecead03b78af4ad5d24685770b021b8fb
dev-murphy <developmentmurphy@gmail.com>
2024-10-02 12:52:30 -0500

    chore: add vue router and unplugin vue router

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useAppStore } from "../stores/app";
+
+const appStore = useAppStore();
+</script>
+
+<template>
+  <div
+    class="min-h-screen flex items-center justify-center bg-neutral-900 text-4xl text-white"
+  >
+    <h1 class="font-inter">Welcome to {{ appStore.appName }} App</h1>
+  </div>
+</template>
