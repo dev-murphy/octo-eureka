fc33d21410fa2d949592506550dd9c8997ad4eae
dev-murphy <developmentmurphy@gmail.com>
2024-10-02 12:31:05 -0500

    chore: add fonts to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satisfy: ["Satisfy", "cursive"],
+        inter: ["Inter", "sans-serif"],
+        gloock: ["Gloock", "serif"],
+      },
+    },
   },
   plugins: [],
 };
