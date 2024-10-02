466a54d07a58826707a267975344372713c9663d
dev-murphy <developmentmurphy@gmail.com>
2024-10-02 12:31:42 -0500

    style: change heading font to inter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-neutral-900 text-4xl text-white">
-    <h1>Welcome to Octo Eureka App</h1>
+    <h1 class="font-inter">Welcome to Octo Eureka App</h1>
   </div>
 </template>
