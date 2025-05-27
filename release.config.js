41eadf0ea0cebc7e1f93582fae64cce4fc116073
Murphy Facey (Dev) <facey.murphy1@gmail.com>
2025-05-27 14:41:54 -0500

    ci: fix issues with release action

---
+++ release.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json", "pnpm-lock.yaml"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        successComment: false,
+        failTitle: false,
+      },
+    ],
+  ],
+};
